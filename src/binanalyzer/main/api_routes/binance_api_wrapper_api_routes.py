c358f058ff747daa503b82643e3a04c3a755374d
Sudhir Dhameliya <sdhameliya@derivativepath.com>
2024-05-08 16:06:33 -0400

    Refactor BinanceAPIWrapper class to improve code readability and maintainability

--- src/binanalyzer/main/api_routes/binance_api_wrapper_api_routes.py
+++ src/binanalyzer/main/api_routes/binance_api_wrapper_api_routes.py
@@ -1,29 +1,261 @@
 from main.binance_api_wrapper.binance_api_wrapper import BinanceAPIWrapper
 
+from typing import Optional, List, Any
 import logging
-from pydantic import BaseModel
-from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel, Field, field_validator, create_model
+from fastapi import APIRouter, HTTPException, Depends, Query, Path, Body
+from typing import Annotated
 
 logging.basicConfig(level=logging.INFO)
 
+binance_api_wrapper_obj = BinanceAPIWrapper()
+
 router = APIRouter()
 
+
+# Coin Pair Price -----------------------------------------------------------------------------------------------
 class CoinPriceResponse(BaseModel):
     isSuccess: bool
-    coinpair_price: float
+    coinpair_price: dict[str, float]
 
-# Dependency to create a new instance of the wrapper class for each request
-def get_binance_api_wrapper():
-    return BinanceAPIWrapper()
 
-@router.post("/coinpair_price", response_model=CoinPriceResponse)
-def coinpair_price(coin_name: str, wrapper: BinanceAPIWrapper = Depends(get_binance_api_wrapper)):
-    """Get the current price of the coinpair"""
+class CoinPriceRequest(BaseModel):
+    coin_name: Optional[str] = Field(
+        default=None, title="coin_name", description="Coin Name", max_length=8
+    )
+    coin_list: Optional[List[str]] = Field(
+        default=None, description="List of Coin Names"
+    )
+
+    @field_validator("coin_name")
+    def validate_coin_name(cls, value):
+        if value is None:
+            return value
+        if value.lower().strip() == "usdt":
+            raise ValueError("Coin name cannot be only USDT")
+        if "usdt" not in value.lower():
+            return value.strip().upper() + "USDT"
+        return value.upper()
+
+    @field_validator("coin_list")
+    def validate_coin_list(cls, value):
+        if value is not None:
+            return [cls.validate_coin_name(coin_name) for coin_name in value]
+        return value
+
+
+@router.get(
+    "/coinpair_price",
+    response_model=CoinPriceResponse,
+    summary="Get the current price of the coinpair or coinpairs.",
+    description="Get the current price of the coinpair or coinpairs. \
+        Provide either a single coin name or a list of coin names.",
+)
+async def get_coinpair_price(
+    coin_name: str = Query(None, title="Coin Name"),
+    coin_list: List[str] = Query(None, title="List of Coin Names"),
+):
     try:
-        logging.info(f"coin_name: {coin_name}")
-        price = wrapper.get_coinpair_price(f"{coin_name.upper()}USDT")
-        logging.info(f"coinpair_price: {price}")
+        request = CoinPriceRequest(coin_name=coin_name, coin_list=coin_list)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-    return {"isSuccess": True, "coinpair_price": price}
+    try:
+        if request.coin_name:
+            price = binance_api_wrapper_obj.get_coinpair_price(request.coin_name)
+            return CoinPriceResponse(
+                isSuccess=True, coinpair_price={request.coin_name: price}
+            )
+        elif request.coin_list:
+            prices_dict = {
+                coin_name: binance_api_wrapper_obj.get_coinpair_price(coin_name)
+                for coin_name in request.coin_list
+            }
+            return CoinPriceResponse(isSuccess=True, coinpair_price=prices_dict)
+        else:
+            raise ValueError("Either coin_name or coin_list must be provided.")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+# No of coins in Spot Wallet -----------------------------------------------------------------------------------------------
+class SpotCoinsResponse(BaseModel):
+    isSuccess: bool
+    free_coins: float = 0.0
+    locked_coins: float = 0.0
+    freeze_coins: float = 0.0
+    withdrawing_coins: float = 0.0
+    ipoable_coins: float = 0.0
+
+
+class SpotCoinsRequest(BaseModel):
+    coin_name: Optional[str] = Field(
+        None,
+        title="coin_name",
+        description="Coin Name",
+        max_length=5,
+    )
+
+    @field_validator("coin_name")
+    def validate_coin_name(cls, value):
+        if value is None:
+            return value
+        return value.upper()
+
+
+@router.get(
+    "/coins_in_spot_wallet",
+    response_model=SpotCoinsResponse,
+    summary="Get the total no of coins in Binance Spot Wallet.",
+)
+async def get_coins_in_spot_wallet(
+    coin_name: str = Query(None, title="Coin Name"),
+):
+    try:
+        request = SpotCoinsRequest(coin_name=coin_name)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    try:
+        response_dict = binance_api_wrapper_obj.get_no_of_coin_from_spot_wallet(
+            request.coin_name
+        )
+        if not response_dict:
+            return SpotCoinsResponse(isSuccess=True)
+
+        return SpotCoinsResponse(
+            isSuccess=True,
+            free_coins=response_dict["free_coins"],
+            locked_coins=response_dict["locked_coins"],
+            freeze_coins=response_dict["freeze_coins"],
+            withdrawing_coins=response_dict["withdrawing_coins"],
+            ipoable_coins=response_dict["ipoable_coins"],
+        )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+# No of coins in Funding Wallet -----------------------------------------------------------------------------------------------
+class FundingCoinsResponse(BaseModel):
+    isSuccess: bool
+    free_coins: float = 0.0
+    locked_coins: float = 0.0
+    freeze_coins: float = 0.0
+    withdrawing_coins: float = 0.0
+
+
+class FundingCoinsRequest(BaseModel):
+    coin_name: Optional[str] = Field(
+        None,
+        title="coin_name",
+        description="Coin Name",
+        max_length=5,
+    )
+
+    @field_validator("coin_name")
+    def validate_coin_name(cls, value):
+        if value is None:
+            return value
+        return value.upper()
+
+
+@router.get(
+    "/coins_in_funding_wallet",
+    response_model=FundingCoinsResponse,
+    summary="Get the total no of coins in Binance Funding Wallet.",
+)
+async def get_coins_in_funding_wallet(
+    coin_name: str = Query(None, title="Coin Name"),
+):
+    try:
+        request = FundingCoinsRequest(coin_name=coin_name)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    try:
+        response_dict = binance_api_wrapper_obj.get_no_of_coin_from_funding_wallet(
+            request.coin_name
+        )
+        if not response_dict:
+            return FundingCoinsResponse(isSuccess=True)
+        return FundingCoinsResponse(
+            isSuccess=True,
+            free_coins=response_dict["free_coins"],
+            locked_coins=response_dict["locked_coins"],
+            freeze_coins=response_dict["freeze_coins"],
+            withdrawing_coins=response_dict["withdrawing_coins"],
+        )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+# No of coins in Earn Wallet -----------------------------------------------------------------------------------------------
+class EarnCoinsResponse(BaseModel):
+    isSuccess: bool
+    total_coins: float = 0.0
+
+
+class EarnCoinsRequest(BaseModel):
+    coin_name: Optional[str] = Field(
+        None,
+        title="coin_name",
+        description="Coin Name",
+        max_length=5,
+    )
+
+    @field_validator("coin_name")
+    def validate_coin_name(cls, value):
+        if value is None:
+            return value
+        return value.upper()
+
+
+@router.get(
+    "/coins_in_earn_wallet",
+    response_model=EarnCoinsResponse,
+    summary="Get the total no of coins in Binance Earn Wallet.",
+)
+async def get_coins_in_earn_wallet(
+    coin_name: str = Query(None, title="Coin Name"),
+):
+    try:
+        request = EarnCoinsRequest(coin_name=coin_name)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    try:
+        total_coins = binance_api_wrapper_obj.get_no_of_coin_from_earn_wallet(
+            request.coin_name
+        )
+        if not total_coins:
+            return EarnCoinsResponse(isSuccess=True)
+        return EarnCoinsResponse(
+            isSuccess=True,
+            total_coins=total_coins,
+        )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+# Convert History -----------------------------------------------------------------------------------------------
+class ConvertHistoryResponse(BaseModel):
+    isSuccess: bool
+    convert_history: List[dict[str, Any]]
+
+
+@router.get(
+    "/get_convert_history",
+    response_model=ConvertHistoryResponse,
+    summary="Get the total no of coins in Binance Earn Wallet.",
+)
+async def get_get_convert_history():
+    try:
+        convert_history = binance_api_wrapper_obj.get_convert_history()
+        if not convert_history:
+            return ConvertHistoryResponse(isSuccess=True, convert_history=[])
+        return ConvertHistoryResponse(
+            isSuccess=True,
+            convert_history=convert_history["list"],
+        )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- src/binanalyzer/main/binance_api_wrapper/binance_api_wrapper.py
+++ src/binanalyzer/main/binance_api_wrapper/binance_api_wrapper.py
@@ -14,8 +14,10 @@ class BinanceAPIWrapper:
 
     def get_coinpair_price(self, coin_pair) -> float:
         """Fetch the current price of the coin_pair"""
+        # make coin pair in upper case
+        coin_pair = coin_pair.upper().strip()
         client = Spot()
-        response = client.ticker_price(symbol=f"{coin_pair}")
+        response = client.ticker_price(symbol=coin_pair)
         return float(response["price"])
 
     def get_total_no_of_coins(self, coin):
@@ -24,12 +26,7 @@ class BinanceAPIWrapper:
         funding = self.get_no_of_coin_from_funding_wallet(coin)
         flexible_earn = self.get_no_of_coin_from_flexible_earn_wallet(coin)
         total_coins = spot + funding + flexible_earn
-        return {
-            "spot": spot,
-            "funding": funding,
-            "flexible_earn": flexible_earn,
-            "total_coins": total_coins,
-        }
+        return total_coins
 
     def get_convert_history(
         self, start_timestamp=None, end_timestamp=None, **params_dict
@@ -37,6 +34,37 @@ class BinanceAPIWrapper:
         """Fetch the convert trade history
         end_timestamp: int(end.timestamp()*1000-1000) send less 1 second to get the last trade
         start_timestamp: {1623824139000} should be have length 13
+
+        sample response from binance api:
+            response['list'] = [
+                {
+                    "quoteId": "9950f21b3f4b43329da49c2532c64719",
+                    "orderId": 1670059876560505617,
+                    "orderStatus": "SUCCESS",
+                    "fromAsset": "LTC",
+                    "fromAmount": "0.32771067",
+                    "toAsset": "USDT",
+                    "toAmount": "24.09672353",
+                    "ratio": "73.5305000000000000",
+                    "inverseRatio": "0.0135997987229789",
+                    "createTime": 1709156558791,
+                    "orderType": "MARKET",
+                    "side": "SELL",
+                },
+                {
+                    "quoteId": "9950f21b3f4b43329da49c2532c64719",
+                    "orderId": 1670059876560505617,
+                    "orderStatus": "SUCCESS",
+                    "fromAsset": "USDT",
+                    "fromAmount": "24.09672353",
+                    "toAsset": "LTC",
+                    "toAmount": "0.32771067",
+                    "ratio": "73.5305000000000000",
+                    "inverseRatio": "0.0135997987229789",
+                    "createTime": 1709156558791,
+                    "orderType": "MARKET",
+                    "side": "BUY",
+            ]
         """
         # Validating the parameters -------------------------
         if start_timestamp:
@@ -87,13 +115,25 @@ class BinanceAPIWrapper:
             timeout=10,
         )
         response = result.json()
+
         if not response:
-            return 0
+            return None
         else:
-            total_coins = 0
+            response_dict = {
+                "free_coins": 0.0,
+                "locked_coins": 0.0,
+                "freeze_coins": 0.0,
+                "withdrawing_coins": 0.0,
+                "ipoable_coins": 0.0,
+            }
             for item in response:
-                total_coins += float(item["free"]) + float(item["locked"])
-            return total_coins
+                response_dict["free_coins"] += float(item["free"])
+                response_dict["locked_coins"] += float(item["locked"])
+                response_dict["freeze_coins"] += float(item["freeze"])
+                response_dict["withdrawing_coins"] += float(item["withdrawing"])
+                response_dict["ipoable_coins"] += float(item["ipoable"])
+
+            return response_dict
 
     def get_no_of_coin_from_funding_wallet(self, coin_name):
         """Get your no of coins in Binance Funding Wallet
@@ -105,24 +145,56 @@ class BinanceAPIWrapper:
             "POST", url, headers=headers, data=payload, timeout=10
         )
         response = result.json()
-        if not response:
-            return 0
-        else:
-            total_coins = 0
-            for item in response:
-                total_coins += float(item["free"]) + float(item["locked"])
-            return total_coins
 
-    def get_no_of_coin_from_flexible_earn_wallet(self, coin_name):
+        if not response:
+            return None
+        else:
+            response_dict = {
+                "free_coins": 0.0,
+                "locked_coins": 0.0,
+                "freeze_coins": 0.0,
+                "withdrawing_coins": 0.0,
+            }
+            for item in response:
+                response_dict["free_coins"] += float(item["free"])
+                response_dict["locked_coins"] += float(item["locked"])
+                response_dict["freeze_coins"] += float(item["freeze"])
+                response_dict["withdrawing_coins"] += float(item["withdrawing"])
+
+            return response_dict
+
+    def get_no_of_coin_from_earn_wallet(self, coin_name):
         """Get your no of coins in Binance Earn Wallet :: Flexible Savings
         coin: ex 'BTC', 'ETH', 'USDT', 'BNB'
+
+        sample response from binance api:
+            {
+                "total": 1,
+                "rows": [
+                    {
+                        "totalAmount": "0.02353327",
+                        "tierAnnualPercentageRate": {"0-0.01BTC": "0.00250000"},
+                        "latestAnnualPercentageRate": "0.0005999",
+                        "asset": "BTC",
+                        "canRedeem": True,
+                        "collateralAmount": "0",
+                        "productId": "BTC001",
+                        "yesterdayRealTimeRewards": "0.00000004",
+                        "cumulativeBonusRewards": "0.00001799",
+                        "cumulativeRealTimeRewards": "0.00001614",
+                        "cumulativeTotalRewards": "0.00019682",
+                        "autoSubscribe": True,
+                    }
+                ],
+            }
         """
         query_string, signature, payload, headers = create_api_parameters(coin_name)
         url = f"{BASE_URL}/sapi/v1/simple-earn/flexible/position?{query_string}&signature={signature}"
         result = requests.request("GET", url, headers=headers, data=payload, timeout=10)
         response = result.json()
+
         if not response:
-            return 0
+            return False
         else:
             total_coins = 0
             for item in response["rows"]:
